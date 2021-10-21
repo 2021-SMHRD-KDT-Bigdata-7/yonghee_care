81eb839a37c7c7561281692d8bc4c1325efd2353
smhrd <smhrd@DESKTOP-6VRK9A5>
2021-10-21 17:13:19 +0900

    로그인!

--- Yonghee_care/src/REGISTER.java
+++ Yonghee_care/src/REGISTER.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
-public class REGISTER {
-
+public class REGISTER{
+	public void register() {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			System.out.print("[1]다마고치등록 [2]다마고치키우기 [3]다마고치상태확인 [4]랭킹확인 [5]종료 >>");
+			
+			int menu = sc.nextInt();
+			if(menu ==1) {
+				System.out.println("");
+			}
+			
+		}
+	}
 }
