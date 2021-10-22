b38058fd2cfb371791646337f32026093c25ceb5
smhrd <smhrd@DESKTOP-FQCRA9H>
2021-10-22 09:49:58 +0900

    selectall2

--- Yonghee_care/src/DAO.java
+++ Yonghee_care/src/DAO.java
@@ -1,20 +1,19 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DAO {
-	
 
-	
 	Connection conn = null;
 	PreparedStatement psmt = null;
 	ResultSet rs = null;
 
 	int result = 0;
-	
-	//DB연결
+
+	// DB연결
 
 	public void getConn() {
 
@@ -35,96 +34,126 @@ public class DAO {
 
 		}
 	}
-	
+
 	// DB종료
-	
-		public void close() {
-			try {
 
-				if (rs != null) {
-					rs.close();
-				}
-				if (psmt != null) {
-					psmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
+	public void close() {
+		try {
 
-				e.printStackTrace();
+			if (rs != null) {
+				rs.close();
 			}
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
 		}
-	
-	//회원가입 아이디 비밀번호 입력
+	}
 
-		// 회원 정보 Insert 메소드
+	// 회원가입 아이디 비밀번호 입력
 
-		public int insert(String id, String pw) {
+	// 회원 정보 Insert 메소드
 
-			// insert를 하기위해서는 데이터베이스 연결이 필요 -> 연결메소드 호출(getConn())
-			getConn();
+	public int insert(String id, String pw) {
 
-			// try~catch로 오류를 잡아 줘야함
-			String sql = "insert into member values(?,?)";
-			
-			try {
+		// insert를 하기위해서는 데이터베이스 연결이 필요 -> 연결메소드 호출(getConn())
+		getConn();
 
-				psmt = conn.prepareStatement(sql);
+		// try~catch로 오류를 잡아 줘야함
+		String sql = "insert into member values(?,?)";
 
-				psmt.setString(1, id);
-				psmt.setString(2, pw);
+		try {
 
+			psmt = conn.prepareStatement(sql);
 
-				result = psmt.executeUpdate();
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				// 데이터베이스 연결 종료
-				close();
-			}
+			result = psmt.executeUpdate();
 
-			return result;
-		}
-	
-		
-		//로그인 
-		// 회원 정보 Select 메소드
-
-		public String[] select(String id, String pw) {
-
-			getConn();
-			String[] getidpw = new String[2];
-
-			try {
-				// 실행할 sql문 작성
-
-				String sql = "select * from member where id=? and pw=?";
-
-				psmt = conn.prepareStatement(sql);
-				
-				psmt.setString(1, id);
-				psmt.setString(2, pw);
-				
-				
-				rs = psmt.executeQuery();
-
-				
-				while (rs.next()) {
-					getidpw[0] = rs.getString(1); // 숫자는 컬럼의 순서
-					getidpw[1] = rs.getString(2);
-				}
-
-			} catch (Exception e) {
-
-				e.printStackTrace();
-
-			} finally {
-				close();
-			}
-			return getidpw;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 데이터베이스 연결 종료
+			close();
 		}
 
+		return result;
+	}
+
+	// 로그인
+	// 회원 정보 Select 메소드
+
+	public String[] select(String id, String pw) {
+
+		getConn();
+		String[] getidpw = new String[2];
+
+		try {
+			// 실행할 sql문 작성
+
+			String sql = "select * from member where id=? and pw=?";
+
+			psmt = conn.prepareStatement(sql);
+
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				getidpw[0] = rs.getString(1); // 숫자는 컬럼의 순서
+				getidpw[1] = rs.getString(2);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+			close();
+		}
+		return getidpw;
+	}
+
+	public void selectAll() {
+
+		getConn();
+
+		try {
+			String sql = "Select * from DAMA order by leb desc";
+
+			psmt = conn.prepareStatement(sql);
+
+			rs = psmt.executeQuery();
+			int num = 1;
+
+			while (rs.next()) {
+				String getNick = rs.getString(1);
+				String getType = rs.getString(2);
+				int getExp = rs.getInt(3);
+				int getLeb = rs.getInt(4);
+				int getEnergy = rs.getInt(5);
+				String getId = rs.getString(6);
+				String getDama_date = rs.getString(7);
+				String getNeeds = rs.getString(8);
+
+				System.out.println(num + "등" + "\t" + "ID : " + getId + "\t" + "NICK : " + getNick + "\t" + "LEVEL : "
+						+ getLeb + "\t" + "EXP : " + getExp + "\t");
+				num++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 3.데이터베이스 닫아주기
+			close();
+		}
+
+	}
 
 }
