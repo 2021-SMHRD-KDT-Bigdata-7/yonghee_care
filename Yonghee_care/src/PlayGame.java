71904d01ff90200b4caa1ef980ade9ab2ca53329
smhrd <smhrd@DESKTOP-6VRK9A5>
2021-10-22 12:26:21 +0900

    푸쉬푸쉬 베이베~

--- Yonghee_care/src/PlayGame.java
+++ Yonghee_care/src/PlayGame.java
@@ -34,7 +34,8 @@ public class PlayGame {
 
 		System.out.println("용히 키우기!!");
 		String[] Ycare = { "밥먹이기", "잠재우기", "놀아주기", "운동하기", "방치우기" };
-		String Ycare2 = Ycare[rd.nextInt(5)];
+		int num = rd.nextInt(5);
+//		String Ycare2 = Ycare[rd.nextInt(5)];
 		boolean flag = true; // 와일문 빠져나오기 위한 불리언 값
 		int stack = 0; // 두번째 틀렸을 시 에어지 깍기 위한 스택 (노형호가 a 라고 함)
 
@@ -47,84 +48,96 @@ public class PlayGame {
 			// 하고싶은거 맞추기 if()문
 
 			// 밥먹이기
-			if (Ycare2.equals(Ycare[menu2-1])) {
-				// 정답- 경험치 40 에너지 ±30
-				idama.eat();
-				flag = false; // while문 빠져나가기
-			} else {
-				System.out.println("용히는 배가 고푼뎁,,, ");
-				if (stack == 1) {
-					// 에너지 깍기
-//					idama.energyDown();
-					flag = false;
+			if(Ycare[num] == "밥먹이기") {
+				if (Ycare[num] == Ycare[menu2-1]) {
+					// 정답- 경험치 40 에너지 ±30
+					idama.eat();
+					flag = false; // while문 빠져나가기
+				} else {
+					System.out.println("용히는 배가 고푼뎁,,, ");
+					if (stack == 1) {
+						// 에너지 깍기
+//						idama.energyDown();
+						flag = false;
+					}
+					stack++;
+					continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
 				}
-				stack++;
-				continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
+			}else if(Ycare[num] == "잠재우기") {
+				// 잠재우기
+				if (Ycare[num] == Ycare[menu2-1]) {
+					// 정답- 경험치 40 에너지 ±30
+					idama.sleep();
+					flag = false;
 
-			}
-
-			// 잠재우기
-			if (Ycare2.equals(Ycare[menu2-1])) {
-				// 정답- 경험치 40 에너지 ±30
-				idama.sleep();
-				flag = false;
-
-			} else {
-				System.out.println("용히는 조용희있고 시푼뎁,,,");
-				if (stack == 1) {
-					// 에너지 깍기
-//					idama.energyDown();
-					flag = false;
+				} else {
+					System.out.println("용히는 조용희있고 시푼뎁,,,");
+					if (stack == 1) {
+						// 에너지 깍기
+//						idama.energyDown();
+						flag = false;
+					}
+					stack++;
+					continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
 				}
-				stack++;
-				continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
-			}
-			// 놀아주기
-			if (Ycare2.equals(Ycare[menu2-1])) {
-				// 정답- 경험치 40 에너지 ±30
-				idama.roll();
-				flag = false;
-			} else {
-				System.out.println("용히는 띰띰해욤,,,");
-				if (stack == 1) {
-					// 에너지 깍기
-//					idama.energyDown();
+			}else if(Ycare[num] == "놀아주기") {
+				// 놀아주기
+				if (Ycare[num] == Ycare[menu2-1]) {
+					// 정답- 경험치 40 에너지 ±30
+					idama.roll();
 					flag = false;
+				} else {
+					System.out.println("용히는 띰띰해욤,,,");
+					if (stack == 1) {
+						// 에너지 깍기
+//						idama.energyDown();
+						flag = false;
+					}
+					stack++;
+					continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
+					
 				}
-				stack++;
-				continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
-				
-			}
-			if (Ycare2.equals(Ycare[menu2-1])) {
-				// 정답- 경험치 40 에너지 ±30
-				idama.exer();
-				flag = false;
-			} else {
-				System.out.println("용히는 씩씩!! 건강!! ");
-				if (stack == 1) {
-					// 에너지 깍기
-//					idama.energyDown();
+			}else if(Ycare[num] == "운동하기") {
+				if (Ycare[num] == Ycare[menu2-1]) {
+					// 정답- 경험치 40 에너지 ±30
+					idama.exer();
 					flag = false;
+				} else {
+					System.out.println("용히는 씩씩!! 건강!! ");
+					if (stack == 1) {
+						// 에너지 깍기
+//						idama.energyDown();
+						flag = false;
+					}
+					stack++;
+					continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
+					
 				}
-				stack++;
-				continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
-				
-			}
-			if (Ycare2.equals(Ycare[menu2-1])) {
-				// 정답- 경험치 40 에너지 ±30
-				idama.clean();
-				flag = false;
-			} else {
-				System.out.println("용히 똥 쌌어요 ㅠㅠ 치워쥬쎄욤 ");
-				if (stack == 1) {
-					// 에너지 깍기
-//					idama.energyDown();
+			}else if(Ycare[num] == "방치우기") {
+				if (Ycare[num] == Ycare[menu2-1]) {
+					// 정답- 경험치 40 에너지 ±30
+					idama.clean();
 					flag = false;
+				} else {
+					System.out.println("용히 똥 쌌어요 ㅠㅠ 치워쥬쎄욤 ");
+					if (stack == 1) {
+						// 에너지 깍기
+//						idama.energyDown();
+						flag = false;
+					}
+					stack++;
+					continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
+					
 				}
-				stack++;
-				continue; // 수민쌤이 맞다고 했음 틀리면 수민쌤책임
-				
+			}else {
+				System.out.println("다시입력하세요");
 			}
+			
+			
+			
+			
+			
+			
 
 		}
 
